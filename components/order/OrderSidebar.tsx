b0b03ee6578a030f099dedeb20626f1af13f3fc5
Alejandro <aletomasperren@gmail.com>
2025-02-12 20:45:57 -0300

    Cliente, Sidebar

--- components/order/OrderSidebar.tsx
+++ components/order/OrderSidebar.tsx
@@ -1,16 +1,25 @@
-import { prisma } from "../../lib/prisma";
+import { prisma } from "../../src/lib/prisma";
+import CategoryIcon from "../ui/CategoryIcon";
 
 async function getCategories() {
   return await prisma.category.findMany();
 }
 
 const OrderSidebar = async () => {
-  const categories = await getCategories()
+  const categories = await getCategories();
 
-  return 
-  <aside className="md:w-72 md:h-screen bg-white">
-    
-  </aside>;
+  return (
+    <aside className="md:w-72 md:h-screen bg-white">
+      <nav className="mt-10">
+        {categories.map(category  => (
+          <CategoryIcon
+          key={category.id}
+          category={category}
+          />
+        ))}
+      </nav>
+    </aside>
+  );
 };
 
 export default OrderSidebar;
